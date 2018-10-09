8a354f8e127b42b2553d3c3a827b1d22129c0666
hirobob <43910044+hirobob@users.noreply.github.com>
2018-10-10 07:10:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -160,9 +160,9 @@ phina.define('MainScene', {
       } else if(point > 2000 && point <= 3000){
         self.exit('result',{score:point,message:'あと少しだ！！がんばれチョアズ！！'});
       } else if(point > 1000 && point <= 2000){
-        self.exit('result',{score:point,message:'サイバイマン以下だな！！まだまだ修業がたりないな！！'});
+        self.exit('result',{score:point,message:'サイバイマン以下だな！！'});
       } else if(point > 0 && point <= 1000){
-        self.exit('result',{score:point,message:'ラディッツ以下だな！！まだまだ修業がたりないな！！'});
+        self.exit('result',{score:point,message:'ラディッツ以下だな！！'});
       } else if(point < 0) {
         self.exit('result',{score:point,message:'テンさんを殺してんじゃねえよ！！'});
       } else  {
