3180ee331ad73346a5ffb3b893c0fc75c3fcd510
hirobob <43910044+hirobob@users.noreply.github.com>
2018-10-17 01:22:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ var ASSETS = {
      bg: 'https://i.imgur.com/8ioMgMn.png',
      senzu_bullet: 'https://i.imgur.com/rnbh0NR.png',
      begeta:'https://i.imgur.com/p2LjRUg.png',
+     title:'https://i.imgur.com/LiYSBTW.png',
   },
    sound: {
     'bgm': 'https://raw.githubusercontent.com/hirobob/chaozu/master/furueru.aac',
@@ -28,7 +29,6 @@ var ASSETS = {
 var SCREEN_WIDTH  = 640;
 var SCREEN_HEIGHT = 960;
 var CHIAOTZU_TOP = 800;
-var chiaotzu; //チャオズ
 var napaNum = 6; // enemyの数
 var napaNum2 = 5; // enemyの数
 var napaNum3 = 3; // enemyの数
@@ -38,7 +38,8 @@ var timeLimit = 20; // 制限時間
 var time;
 var senzuBtn;
 var isSenzu;
-var bagetabulletGroup;
+var chiaotzu; //チャオズ
+var bageta;
 
 // MainScene クラスを定義
 phina.define('Scene01', {
@@ -165,16 +166,12 @@ phina.define('Scene01', {
     timeCount = timeLimit - timed;
     this.label_time.text = '残り時間：' + timeCount;
     
-    if(point>1000){
-    this.senzuBtn.visible = true;
-    }
     // ドドンパの当たり判定
     this.hitNapaBullet();
     this.hitTenBullet();
     
     // 移動判定
     this.enemyGroup.children.each(function(enemy) {
-      //enemy.rotation -= 1;
       if(enemy.level==='napa'){
         if(enemy.x < 0){
           enemy.x =600;
@@ -184,51 +181,43 @@ phina.define('Scene01', {
         enemy.x =600;
       }
     });
-    
     this.tenGroup.children.each(function(ten) {
-    //ten.x += 5;
-    if(ten.x > 600){
-      ten.x =0;
-    }
+      if(ten.x > 600){
+        ten.x =0;
+      }
     });
     
     // ポイント更新
     this.label_point.text = point;
     
-    var self = this;
+    // 集計
     if(timeCount <= 0 ){
       
-      //SoundManager.stopMusic();
-      this.exit();
+      SoundManager.stopMusic();
+      
       if(point > 3500){
-        //this.exit();
-        
-        
+        this.exit();
       } else if(point >= 3000 && point < 3500){
-        //self.exit('result',{score:point,message:'テンさんを助けてくれてありがとう！'});
+        app.replaceScene(EndScene(point,'テンさんを助けてくれてありがとう！'))
       } else if(point >= 2000 && point < 3000){
-        //self.exit('result',{score:point,message:'あと少しだ！がんばれチョアズ！'});
+        app.replaceScene(EndScene(point,'あと少しだ！がんばれチョアズ！'))
       } else if(point >= 1000 && point < 2000){
-        //self.exit('result',{score:point,message:'サイバイマン以下だな！クズ野郎！'});
+        app.replaceScene(EndScene(point,'サイバイマン以下だな！クズ野郎！'))
       } else if(point > 0 && point < 1000){
-
-        //self.exit('result',{score:point,message:'ラディッツ以下だな！！残業詐欺すんな！'});
+        app.replaceScene(EndScene(point,'ラディッツ以下だな！！残業詐欺すんな！'))
       } else if(point < 0) {
-        //SoundManager.play('oitekita');
-        //self.exit('result',{score:point,message:'テンさんを殺してんじゃねえよ！！'});
+        // SoundManager.play('oitekita');
+        this.exit();
+        //app.replaceScene(EndScene(point,'テンさんを殺してんじゃねえよ！！'))
       } else  {
-        //self.exit('result',{score:point,message:'やる気あんのかよ！！'});
+        app.replaceScene(EndScene(point,'やる気あんのかよ！！'))
       }
     }
-    
     
   },
   
   // タッチ判定がtrueの時にタッチされたら実行される処理
   onpointstart: function(e) {
-    //SoundManager.play('bgmDodo');
-    
-
   },
   
   // タッチ判定がtrueの時にタッチされ終わったときに実行される処理
@@ -246,10 +235,8 @@ phina.define('Scene01', {
 });
 
 
-// MainScene クラスを定義
+// ステージ2「ベジータ」
 phina.define('Scene02', {
-  
-  // DisplaySceneクラスを継承
   superClass: 'DisplayScene',
   
   // コンストラクタ
@@ -264,74 +251,79 @@ phina.define('Scene02', {
     // 背景
     Sprite('bg').addChildTo(this).setPosition(this.gridX.center(), this.gridY.center());
     
-    // ゲージサンプル
-    this.value1 = 100;
+    this.begeEnetimer = 0;
+    
+    // ベジータの体力ゲージ
     this.gauge1 = Gauge({
-      x: 320, y: 50,        // x,y座標
-      width: 450,            // 横サイズ
-      height: 30,            // 縦サイズ
+      x: 320, y: 50,         // x,y座標
+      width: 500,            // 横サイズ
+      height: 15,            // 縦サイズ
       cornerRadius: 10,      // 角丸み
-      maxValue: 100,         // ゲージ最大値
-      value: this.value1,         // ゲージ初期値
+      maxValue: 400,         // ゲージ最大値
+      value: 400,            // ゲージ初期値
       fill: 'white',         // 後ろの色
       gaugeColor: 'skyblue', // ゲージ色
       stroke: 'silver',      // 枠色
       strokeWidth: 5,        // 枠太さ
     }).addChildTo(this);
     
-        // ゲージサンプル
-    this.value2 = 100;
+    // チャオズの体力ゲージ
     this.gauge2 = Gauge({
       x: 320, y: 910,        // x,y座標
       width: 250,            // 横サイズ
-      height: 30,            // 縦サイズ
+      height: 15,            // 縦サイズ
       cornerRadius: 10,      // 角丸み
-      maxValue: 100,         // ゲージ最大値
-      value: this.value2,         // ゲージ初期値
+      maxValue: 200,         // ゲージ最大値
+      value: 200,            // ゲージ初期値
       fill: 'white',         // 後ろの色
       gaugeColor: 'skyblue', // ゲージ色
       stroke: 'silver',      // 枠色
       strokeWidth: 5,        // 枠太さ
     }).addChildTo(this);
     
-    this.gauge1.onempty = function(){
-      //this.exit();
-    }; // 空になった時
     
-    this.gauge2.onempty = function(){
-      SoundManager.play('kitanehanabi');
-    }; // 空になった時
-    
-    // ベジータを追加
-    this.bagetaGroup = DisplayElement().addChildTo(this);
-    Begeta(0,100,8).addChildTo(this.bagetaGroup);
-    
-    // チャオズを追加
-    chiaotzu = Chiaotzu().addChildTo(this);
-    
-    // ドドンパグループ作成
-    this.bulletGroup = DisplayElement().addChildTo(this);
-        // ドドンパグループ作成
+    // ベジータ
+    bageta = Begeta(0, 100, 5).addChildTo(this);
     this.bagetabulletGroup = DisplayElement().addChildTo(this);
+    
+    // チャオズ
+    chiaotzu = Chiaotzu().addChildTo(this);
+    this.bulletGroup = DisplayElement().addChildTo(this);
   },
   
-    //update時の処理
+  //update時の処理
   update: function(app) {
     
-        // ドドンパの当たり判定
+    // ベジータのビーム発射
+    ++this.begeEnetimer
+    if (this.begeEnetimer % 30 === 0) {
+      for (var i = 0, n = (this.begeEnetimer / 300); i < n; ++i) {
+        var enemy = Bullet_Bageta('chiaotzu_bullet',40,40).addChildTo(this.bagetabulletGroup);
+        enemy.x = Math.randint(0, bageta.x);
+        enemy.y = bageta.height+bageta.y ;
+      }
+    }
+    
+    // ビーム当たり判定
     this.hitBegetaBullet();
     this.hitChiaotzuBullet();
-    this.bagetaGroup.children.each(function(bageta) {
-    if(bageta.x > 600){
-      bageta.x =0;
-    }
-    });
+    
+    // ベジータゲージが「0」になったとき
+    this.gauge1.onempty = function(){
+      SoundManager.stopMusic();
+      app.replaceScene(EndScene(0,'続き開発中'))
+    };
+    // チャオズゲージが「0」になったとき
+    this.gauge2.onempty = function(){
+      SoundManager.stopMusic();
+      SoundManager.play('kitanehanabi');
+      app.replaceScene(EndScene(-9999999,'D助ぶっ潰す！'))
+    };
   },
   
   // ベジータとビームの当たり判定
   hitBegetaBullet: function() {
     var self = this;
-    this.bagetaGroup.children.each(function(bageta) {
     self.bulletGroup.children.each(function(bullet) {
         var c1 = Circle(bageta.x,bageta.y,40);
         var c3 = Circle(bullet.x,bullet.y,20);
@@ -340,17 +332,15 @@ phina.define('Scene02', {
           //SoundManager.play('sayonaraten');
           bullet.remove();
           var value;
-          value = self.gauge1.value - 5;
+          value = self.gauge1.value - 10;
           self.gauge1.value = value;
         }
     });
-  });
   },
   
-    // ベジータとビームの当たり判定
+  // チャオズとビームの当たり判定
   hitChiaotzuBullet: function() {
     var self = this;
-  
     self.bagetabulletGroup.children.each(function(bullet) {
         var c1 = Circle(chiaotzu.x,chiaotzu.y,40);
         var c3 = Circle(bullet.x,bullet.y,20);
@@ -359,43 +349,33 @@ phina.define('Scene02', {
           //SoundManager.play('sayonaraten');
           bullet.remove();
           var value;
-          value = self.gauge2.value - 20;
+          value = self.gauge2.value - 10;
           self.gauge2.value = value;
         }
     });
   },
   
-    // タッチ判定がtrueの時にタッチされ終わったときに実行される処理
+  // タッチ判定がtrueの時にタッチされ終わったときに実行される処理
   onpointend: function(e) {
-         bul2= Bullet_Bageta('chiaotzu_bullet',30,30);
-    bul2.addChildTo(this.bagetabulletGroup).setPosition(200,400);
      bul= Bullet('chiaotzu_bullet',30,30);
      bul.addChildTo(this.bulletGroup).setPosition(chiaotzu.x,chiaotzu.y);
   }
+  
 });
 
   
-//チャオズクラスを定義
+// チャオズクラス
 phina.define('Chiaotzu', {
-  
-  //Spriteクラスを継承
   superClass: 'Sprite',
   
-  //コンストラクタ
   init: function() {
-    //親クラス初期化
     this.superInit('chiaotzu', 64, 64);
-    // タッチ可能にする
-    this.setInteractive(true); 
-    //位置をセット
+    this.setInteractive(true); // タッチ可能にする
     this.setPosition(SCREEN_WIDTH / 6,SCREEN_HEIGHT / 6);
-    //y座標の基準点を一番上に
-    this.setOrigin(0.5, 0);
-    //左右反転
-    this.scaleX = -1;
+    this.setOrigin(0.5, 0);    //y座標の基準点を一番上に
+    this.scaleX = -1;          //左右反転
   },
   
-  //update時の処理
   update: function(app) {
     
     if(isSenzu){
@@ -425,16 +405,10 @@ phina.define('Chiaotzu', {
     }
   },
   
-    //onpointend時の処理
-  onpointend: function(app){
-
-  },
-  
 });
 
 // チャオズビームクラス
 phina.define('Bullet',{
-  
   superClass: 'Sprite',
   
   init:function(image,player_x,player_y){
@@ -451,13 +425,10 @@ phina.define('Bullet',{
   
 });
 
-//天津飯クラスを定義
+// 天津飯クラス
 phina.define('Ten', {
-  
-  //Spriteクラスを継承
   superClass: 'Sprite',
   
-  //コンストラクタ
   init: function(setPositionX, setPositionY,moveSpeed) {
     this.superInit('ten', 64, 64);//親クラス初期化
     this.setPosition(setPositionX, setPositionY);//位置をセット
@@ -466,19 +437,12 @@ phina.define('Ten', {
     this.physical.velocity.x = moveSpeed; // 移動方向＆いスピード
   },
   
-  //update時の処理
-  update: function(app) {
-  }
-  
 });
 
-//ナッパクラスを定義
+// ナッパクラス
 phina.define('Napa', {
-  
-  //Spriteクラスを継承
   superClass: 'Sprite',
   
-  //コンストラクタ
   init: function(setPositionX, setPositionY,level,point,moveSpeed) {
     this.superInit(level, 90, 90);//親クラス初期化
     this.setPosition(setPositionX, setPositionY);//位置をセット(X,Y)
@@ -487,81 +451,116 @@ phina.define('Napa', {
     this.physical.velocity.x = moveSpeed;
     this.level = level;
   },
-
-  //update時の処理
-  update: function(app) {
-  }
     
 });
 
-//ベジータクラスを定義
+// ベジータクラス
 phina.define('Begeta', {
-  
-  //Spriteクラスを継承
   superClass: 'Sprite',
-  
-  //コンストラクタ
+
   init: function(setPositionX, setPositionY,moveSpeed) {
-    this.superInit('begeta', 200, 400);//親クラス初期化
-    this.setPosition(setPositionX, setPositionY);//位置をセット
-    this.setOrigin(0.5, 0); //y座標の基準点を一番上に
-    this.physical.velocity.x = moveSpeed; // 移動方向＆いスピード
+    this.superInit('begeta', 125, 250);
+    this.setPosition(setPositionX, setPositionY);
+    this.setOrigin(0.5, 0);
+    this.physical.velocity.x  = moveSpeed; 
   },
   
   //update時の処理
   update: function(app) {
-
-
+    if(bageta.x > 600){
+      bageta.x =0;
+    }
   }
   
 });
 
 // ベジータビームクラス
 phina.define('Bullet_Bageta',{
-  
   superClass: 'Sprite',
   
-  init:function(image,player_x,player_y){
-    this.superInit(image,player_x,player_y);
-    this.physical.velocity.y = 15;
-  },
-  
-  //update時の処理
-  update: function(){
-    //if (this.top < 0) {
-    //  this.remove();
-    //}
+  init:function(image, player_x, player_y){
+    this.superInit(image, player_x, player_y);
+    this.physical.velocity.y = 13;
   },
   
 });
-// メイン処理
+
+
+/**
+ * エンド画面
+ */
+phina.define("EndScene", {
+  superClass : "phina.game.ResultScene",
+
+  init : function(point, msg) {
+    
+    var RESULT_PARAM = {
+      score:    point,
+      message:  msg,
+      hashtags: ["omatoro", "phina.js"],
+      url:      "https://github.com/omatoro/phina.js_tutorial_avoidgame/",
+      width:    SCREEN_WIDTH,
+      height:   SCREEN_HEIGHT,
+    };
+    
+    this.superInit(RESULT_PARAM);
+  },
+
+  // Backボタンを押したらTitleSceneに戻る
+  onnextscene: function (e) {
+    e.target.app.replaceScene(TitleScene());
+  },
+});
+
+/**
+ * タイトル画面
+ */
+phina.define("TitleScene", {
+  superClass : "phina.game.TitleScene",
+  
+  init: function() {
+    this.superInit({
+      backgroundColor: 'rgb(20,20,20)',
+      width: SCREEN_WIDTH,
+      height: SCREEN_HEIGHT,
+    });
+    
+    Sprite('title').addChildTo(this).setPosition(this.gridX.center(), this.gridY.center()+50);
+    
+    Label({
+      text: '天さん死なないで',
+      fontSize: 64,
+      fill: 'white',
+    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(4)-150);
+
+    Label({
+      text: "TOUCH START",
+      fontSize: 64,
+      fill: 'white',
+    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(12)).tweener.fadeOut(1000).fadeIn(500).setLoop(true).play();
+  },
+  
+});
+
+/**
+ * メイン処理
+ */
 phina.main(function() {
   
   // アプリケーション生成
   var app = GameApp({
-    //title: '天さん死なないで',
-    // メインシーンから開始する
-    //startLabel: location.search.substr(1).toObject().scene || 'title', 
-        scenes: [
-      {
-        className: 'Scene01',
-        label: 'scene01',
-        nextLabel: 'scene02',
-      },
-      {
-        className: 'Scene02',
-        label: 'scene02',
-        nextLabel: 'scene01',
-      },
-    ],
-    startLabel: 'scene01',
+    scenes: [{className: 'TitleScene', label: 'title', nextLabel: 'scene01',},
+             {className: 'Scene01', label: 'scene01', nextLabel: 'scene02',},
+             {className: 'Scene02', label: 'scene02', nextLabel: 'scene01',},
+            ],
+    startLabel: 'title',
     width: SCREEN_WIDTH,     // スクリーンの横幅
     height: SCREEN_HEIGHT,   // スクリーンの縦幅
     backgroundColor: '#444', // スクリーンの背景色
     autoPause: true,         // 初期ポーズをするか
-    debug: false,            // デバッグモードにするか
+    debug: true,             // デバッグモードにするか
     fps: 60,                 // 1秒間に画面を更新する回数
-    assets: ASSETS         //アセット
+    assets: ASSETS           // アセット
   });
   
   // iphonで音を出すための対応
